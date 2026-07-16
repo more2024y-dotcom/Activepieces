0f1b88198ef19bf8b4ca4cb88c3a83bdeec5b971
more2024y-dotcom <more2024y@gmail.com>
2026-07-16 09:51:08 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,12 @@
 let completed = 15;
 let remaining = 25;
 
-
 function completeTask(button){
 
-    button.innerHTML = "Completed";
+    button.innerHTML = "Completed ✓";
     button.style.background = "green";
+
+    button.disabled = true;
 
     completed++;
     remaining--;
