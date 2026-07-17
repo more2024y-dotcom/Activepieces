fef0f5fc8e4ccc5a18a8de9fe11fd7f52d6dca48
more2024y-dotcom <more2024y@gmail.com>
2026-07-18 01:04:42 +0800

    Update user-task.js

--- js/user-task.js
+++ js/user-task.js
@@ -5,7 +5,6 @@ function loadTasks(){
 
 let container = document.querySelector(".tasks");
 
-
 container.innerHTML = "";
 
 
@@ -13,8 +12,12 @@ let savedTasks =
 JSON.parse(localStorage.getItem("tasks")) || [];
 
 
+savedTasks.forEach((task, index)=>{
 
-savedTasks.forEach((task)=>{
+
+if(task.status === "Completed"){
+    completed++;
+}
 
 
 container.innerHTML += `
@@ -36,8 +39,18 @@ Assigned: ${task.user}
 </p>
 
 
-<button onclick="completeTask(this)">
-Start Task
+<p>
+Status: ${task.status}
+</p>
+
+
+<button 
+onclick="completeTask(${index}, this)"
+${task.status === "Completed" ? "disabled" : ""}
+>
+
+${task.status === "Completed" ? "Completed ✓" : "Start Task"}
+
 </button>
 
 
@@ -48,34 +61,65 @@ Start Task
 });
 
 
+updateProgress();
+
 }
 
 
 
 
-function completeTask(button){
+function completeTask(index, button){
 
 
-button.innerHTML="Completed ✓";
+let savedTasks =
+JSON.parse(localStorage.getItem("tasks")) || [];
 
 
-button.disabled=true;
+
+savedTasks[index].status = "Completed";
+
+
+
+localStorage.setItem(
+"tasks",
+JSON.stringify(savedTasks)
+);
+
+
+
+button.innerHTML = "Completed ✓";
+
+button.disabled = true;
+
 
 
 completed++;
 
 
-document.getElementById("completed").innerHTML = completed;
+updateProgress();
+
+
+}
+
+
+
+
+function updateProgress(){
+
+
+document.getElementById("completed").innerHTML =
+completed;
 
 
 let remaining = 40 - completed;
 
 
-document.getElementById("remaining").innerHTML = remaining;
+document.getElementById("remaining").innerHTML =
+remaining;
 
 
 
-let percent = Math.floor((completed/40)*100);
+let percent = Math.floor((completed / 40) * 100);
 
 
 
@@ -89,7 +133,6 @@ percent + "% Completed";
 
 
 }
-
 
 
 
