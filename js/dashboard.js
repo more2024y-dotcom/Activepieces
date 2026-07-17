fa618b82394a0b61f5645f61fd187c170ff67e3e
more2024y-dotcom <more2024y@gmail.com>
2026-07-17 23:56:30 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,84 +1,5 @@
-let completed = 15;
+let completed = 0;
 let total = 40;
-
-
-let tasks = [
-
-{
-title: "Task #1",
-category: "AI Training Review",
-description: "Review and verify AI training data."
-},
-
-{
-title: "Task #2",
-category: "Customer Verification",
-description: "Verify customer information and complete review."
-},
-
-{
-title: "Task #3",
-category: "Platform Review",
-description: "Review platform data accuracy."
-}
-
-];
-
-
-
-function loadTasks(){
-
-let container = document.querySelector(".tasks");
-
-container.innerHTML = "";
-
-
-tasks.forEach((task)=>{
-
-
-container.innerHTML += `
-
-<div class="task">
-
-<h3>${task.title}</h3>
-
-<p>
-${task.category}
-<br>
-${task.description}
-</p>
-
-
-<button onclick="completeTask(this)">
-Start Task
-</button>
-
-
-</div>
-
-`;
-
-});
-
-
-}
-
-
-
-function completeTask(button){
-
-button.innerHTML = "Completed ✓";
-
-button.style.background = "green";
-
-button.disabled = true;
-
-
-completed++;
-
-updateProgress();
-
-}
 
 
 
@@ -88,15 +9,20 @@ function updateProgress(){
 let percent = Math.floor((completed / total) * 100);
 
 
-document.getElementById("completed").innerHTML = completed;
+
+document.getElementById("completed").innerHTML =
+completed;
+
 
 
 document.getElementById("remaining").innerHTML =
 total - completed;
 
 
+
 document.getElementById("progress").style.width =
 percent + "%";
+
 
 
 document.getElementById("progress-text").innerHTML =
@@ -104,7 +30,3 @@ percent + "% Completed";
 
 
 }
-
-
-
-loadTasks();
