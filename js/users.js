317b2c4183e131a42f9ec9491e564f0bafcb4de2
more2024y-dotcom <more2024y@gmail.com>
2026-07-18 02:29:39 +0800

    Update users.js

--- js/users.js
+++ js/users.js
@@ -1,1 +1,60 @@
+let users = JSON.parse(localStorage.getItem("users")) || [];
 
+
+function addUser(){
+
+let username =
+document.getElementById("newUser").value;
+
+
+if(username === ""){
+    alert("Enter username");
+    return;
+}
+
+
+users.push(username);
+
+
+localStorage.setItem(
+"users",
+JSON.stringify(users)
+);
+
+
+loadUsers();
+
+}
+
+
+
+function loadUsers(){
+
+let list =
+document.getElementById("userList");
+
+
+if(!list) return;
+
+
+list.innerHTML = "";
+
+
+users.forEach((user)=>{
+
+
+list.innerHTML += `
+
+<p>
+${user}
+</p>
+
+`;
+
+});
+
+
+}
+
+
+loadUsers();
