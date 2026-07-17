c87fcd227ac294feb683f8e711fc03fac44806fe
more2024y-dotcom <more2024y@gmail.com>
2026-07-17 23:25:28 +0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,18 +2,81 @@ let completed = 15;
 let total = 40;
 
 
+let tasks = [
+
+{
+title: "Task #1",
+category: "AI Training Review",
+description: "Review and verify AI training data."
+},
+
+{
+title: "Task #2",
+category: "Customer Verification",
+description: "Verify customer information and complete review."
+},
+
+{
+title: "Task #3",
+category: "Platform Review",
+description: "Review platform data accuracy."
+}
+
+];
+
+
+
+function loadTasks(){
+
+let container = document.querySelector(".tasks");
+
+container.innerHTML = "";
+
+
+tasks.forEach((task)=>{
+
+
+container.innerHTML += `
+
+<div class="task">
+
+<h3>${task.title}</h3>
+
+<p>
+${task.category}
+<br>
+${task.description}
+</p>
+
+
+<button onclick="completeTask(this)">
+Start Task
+</button>
+
+
+</div>
+
+`;
+
+});
+
+
+}
+
+
+
 function completeTask(button){
 
-    button.innerHTML = "Completed ✓";
+button.innerHTML = "Completed ✓";
 
-    button.style.background = "green";
+button.style.background = "green";
 
-    button.disabled = true;
+button.disabled = true;
 
 
-    completed++;
+completed++;
 
-    updateProgress();
+updateProgress();
 
 }
 
@@ -21,21 +84,27 @@ function completeTask(button){
 
 function updateProgress(){
 
-    let percent = Math.floor((completed / total) * 100);
+
+let percent = Math.floor((completed / total) * 100);
 
 
-    document.getElementById("completed").innerHTML = completed;
+document.getElementById("completed").innerHTML = completed;
 
 
-    document.getElementById("remaining").innerHTML =
-    total - completed;
+document.getElementById("remaining").innerHTML =
+total - completed;
 
 
-    document.getElementById("progress").style.width =
-    percent + "%";
+document.getElementById("progress").style.width =
+percent + "%";
 
 
-    document.getElementById("progress-text").innerHTML =
-    percent + "% Completed";
+document.getElementById("progress-text").innerHTML =
+percent + "% Completed";
+
 
 }
+
+
+
+loadTasks();
