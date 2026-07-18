395feb1073d98499367e343e4c6265a1df46c874
more2024y-dotcom <more2024y@gmail.com>
2026-07-19 00:13:53 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
+const supabaseUrl = "YOUR_SUPABASE_PROJECT_URL";
 
+const supabaseKey = "YOUR_SUPABASE_ANON_KEY";
+
+
+const supabaseClient = supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
