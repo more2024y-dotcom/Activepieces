0528826a1798849999fa54bbf1f94437fd556692
more2024y-dotcom <more2024y@gmail.com>
2026-07-19 01:10:04 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,10 @@
 const supabaseUrl = "https://sbrldbbdomfydlynxlsp.supabase.co";
 
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicmxkYmJkb21meWRseW54bHNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzMDYxNjMsImV4cCI6MjA5OTg4MjE2M30.E5NbhpW-kqRkjyx_JuZgIefGxc3F1R9nqs51i-RqcTY";
-
+const supabaseKey = "sb_publishable_UCyYCPiO-Xnbwn0f92z0fw_1Xda2C-s";
 
 const supabaseClient = supabase.createClient(
     supabaseUrl,
     supabaseKey
 );
+
+console.log("Supabase client:", supabaseClient);
