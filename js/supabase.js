a031f8afef3befbcc82d4f91a092889daac7fb1c
more2024y-dotcom <more2024y@gmail.com>
2026-07-19 02:04:12 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,7 @@
 const supabaseUrl = "https://sbrldbbdomfydlynxlsp.supabase.co";
 
-const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicmxkYmJkb21meWRseW54bHNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzMDYxNjMsImV4cCI6MjA5OTg4MjE2M30.E5NbhpW-kqRkjyx_JuZgIefGxc3F1R9nqs51i-RqcTY
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicmxkYmJkb21meWRseW54bHNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzMDYxNjMsImV4cCI6MjA5OTg4MjE2M30.E5NbhpW-kqRkjyx_JuZgIefGxc3F1R9nqs51i-RqcTY";
+
 
 const supabaseClient = supabase.createClient(
     supabaseUrl,
@@ -8,5 +9,4 @@ const supabaseClient = supabase.createClient(
 );
 
 
-console.log("Supabase URL:", supabaseUrl);
-console.log("Key starts:", supabaseKey.substring(0,15));
+console.log("Supabase connected:", supabaseClient);
