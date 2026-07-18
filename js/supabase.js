5230dba305e3c8f379eba760c71ea6147cb89f34
more2024y-dotcom <more2024y@gmail.com>
2026-07-19 02:00:02 +0800

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,12 +1,13 @@
 const supabaseUrl = "https://sbrldbbdomfydlynxlsp.supabase.co";
 
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicmxkYmJkb21meWRseW54bHNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzMDYxNjMsImV4cCI6MjA5OTg4MjE2M30.E5NbhpW-kqRkjyx_JuZgIefGxc3F1R9nqs51i-RqcTY";
+const supabaseKey = "YOUR_ANON_KEY";
 
 
 const supabaseClient = supabase.createClient(
     supabaseUrl,
     supabaseKey
 );
+
+
 console.log("Supabase URL:", supabaseUrl);
-console.log("Supabase Key:", supabaseKey.substring(0,20));
-console.log("Supabase Client:", supabaseClient);
+console.log("Key starts:", supabaseKey.substring(0,15));
